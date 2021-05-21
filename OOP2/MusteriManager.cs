78de05b152c5cfc9a7850dabd7122531492785fb
saidmusabk <78953454+saidmusabk@users.noreply.github.com>
2021-05-21 20:39:23 +0300

    23 ocak ders sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //İs sınıfları // Business classes
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+
+        }
+    }
+}
